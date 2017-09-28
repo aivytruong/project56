1012eedceabddda0a0c3345a0deec22c345b9bf2
catalinaandreitacarstoiu <0925887@hr.nl>
2017-09-28 12:27:55 +0200

    added button to product page

--- ClientApp/routes.tsx
+++ ClientApp/routes.tsx
@@ -1,12 +1,15 @@
+
 import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { Sets } from './components/Sets';
 import { Sale } from './components/Sale';
+import { Product } from './components/Product';
 
 export const routes = <Layout>
     <Route exact path='/' component={ Home } />
     <Route path='/sale' component={ Sale } />
     <Route path='/sets' component={ Sets } />
+    <Route path='/product' component={ Product } />
 </Layout>;
