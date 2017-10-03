f447923f10d319bfb6ad40aed14dd59054c87c51
gregoriec <gregorie_anti@live.nl>
2017-10-03 12:53:32 +0200

    nav bar niet af

--- ClientApp/components/NavMenu.tsx
+++ ClientApp/components/NavMenu.tsx
@@ -4,19 +4,7 @@ import { Link, NavLink } from 'react-router-dom';
 export class NavMenu extends React.Component<{}, {}> {
     public render() {
         return <div className='main-nav'>
-                <div className='navbar navbar-inverse'>
-                <div className='navbar-header'>
-                    <button type='button' className='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>
-                        <span className='sr-only'>Toggle navigation</span>
-                        <span className='icon-bar'></span>
-                        <span className='icon-bar'></span>
-                        <span className='icon-bar'></span>
-                    </button>
-                    <Link className='navbar-brand' to={ '/' }>project56</Link>
-                </div>
-                <div className='clearfix'></div>
-                <div className='navbar-collapse collapse'>
-                    <ul className='nav navbar-nav'>
+                    <ul>
                         <li>
                             <NavLink to={ '/' } exact activeClassName='active'>
                                 <span className='glyphicon glyphicon'></span> Home
@@ -32,9 +20,15 @@ export class NavMenu extends React.Component<{}, {}> {
                                 <span className='glyphicon glyphicon'></span> Sets
                             </NavLink>
                         </li>
+                        <li className='dropdown'>
+                            <img src="https://png.icons8.com/lego-head/Dusk_Wired/1600" width={20} height={20}/>
+                            <div className='dropdown-content'>
+                                <span>I am the best around</span>
+                                <span>I am the best around</span>
+                                <span>I am the best around</span>
+                            </div>
+                        </li>
                     </ul>
                 </div>
-            </div>
-        </div>;
     }
 }
