4289bedaf992b32518cd660c199a230cd43a801c
shokoefa <sho.ha@hotmail.com>
2017-10-03 12:37:42 +0200

    images klikbaar gemaakt en buttons aangepast zoals back enzo en add to cart

--- ClientApp/components/Sale.tsx
+++ ClientApp/components/Sale.tsx
@@ -14,7 +14,7 @@ export class Sale extends React.Component<RouteComponentProps<{}>> {
             <div>Welcome to the sale page! Here you will find the best sales for LEGO </div>
             <br/>
             <h2>Starwars</h2>
-            <img src="https://images.brickset.com/sets/large/10188-1.jpg?200807260532" width={600} height={300}/>
+            <NavLink to={ '/product' }  activeClassName='active'> <button><img src="https://images.brickset.com/sets/large/10188-1.jpg?200807260532" width={600} height={300}/></button> </NavLink>
             <br/>
             <h3>From price: €50,- to €25,-</h3>
             <NavLink to={ '/product' }  activeClassName='active'> <button>Lees meer</button> </NavLink>
@@ -24,7 +24,7 @@ export class Sale extends React.Component<RouteComponentProps<{}>> {
             <br/>
             <br/>
             <h2>Starwars Sandcrawler</h2>
-            <img src="http://cdn-static.denofgeek.com/sites/denofgeek/files/styles/article_width/public/2017/02/2._sandcrawler.jpg" width={600} height={300}/>
+            <NavLink to={ '/product' }  activeClassName='active'> <button><img src="http://cdn-static.denofgeek.com/sites/denofgeek/files/styles/article_width/public/2017/02/2._sandcrawler.jpg" width={600} height={300}/></button> </NavLink>
             <br/>
             <h3>From price: €70,- to €40,-</h3>
             <NavLink to={ '/product' }  activeClassName='active'> <button>Lees meer</button> </NavLink>
@@ -33,12 +33,13 @@ export class Sale extends React.Component<RouteComponentProps<{}>> {
             <br/>
             <br/>
             <h2>Starwars: The Force Awakens</h2>
-            <img src="https://lc-www-live-s.legocdn.com/r/www/r/starwars/-/media/franchises/starwars2015/apps/forceawakensmobileedition/1_keyart_1488x837.jpg" width={600} height={300}/>
+            <NavLink to={ '/product' }  activeClassName='active'> <button><img src="https://lc-www-live-s.legocdn.com/r/www/r/starwars/-/media/franchises/starwars2015/apps/forceawakensmobileedition/1_keyart_1488x837.jpg" width={600} height={300}/></button> </NavLink>
             <br/>
             <h3>From price: €90,- to €60,- </h3>
             <NavLink to={ '/product' }  activeClassName='active'> <button>Lees meer</button> </NavLink>
             <br/>
             <br/>
+            
         </div>;
     }
 
