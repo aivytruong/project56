4469fe328c93a4a1ef12bab8bd6f9ef0c40c6b3a
shokoefa <sho.ha@hotmail.com>
2017-10-12 12:28:22 +0200

    losseblokken en sets klikbare plaatjes

--- ClientApp/components/Sets.tsx
+++ ClientApp/components/Sets.tsx
@@ -16,25 +16,25 @@ export class Sets extends React.Component<RouteComponentProps<{}>> {
             <br/>
 
             <div className="categories">
-            <img src="https://pbs.twimg.com/media/Cl9q5-1WYAAfHL7.jpg:large" width={300} height={200}/>
+            <NavLink to={ '/product' }  activeClassName='active'> <button><img src="https://pbs.twimg.com/media/Cl9q5-1WYAAfHL7.jpg:large" width={300} height={200}/></button> </NavLink>
             <br/>
             <NavLink to={ '/product' }  activeClassName='active'> <button>LEGO NINJAGO</button> </NavLink>     
             </div>
 
             <div className="categories">
-            <img src="http://cdn-static.denofgeek.com/sites/denofgeek/files/styles/main_wide/public/2017/05/lego_main.jpg?itok=OYmDXcmD" width={300} height={200}/>
+            <NavLink to={ '/StarwarsProductPage' }  activeClassName='active'> <button><img src="http://cdn-static.denofgeek.com/sites/denofgeek/files/styles/main_wide/public/2017/05/lego_main.jpg?itok=OYmDXcmD" width={300} height={200}/></button> </NavLink>
             <br/>
             <NavLink to={ '/StarwarsProductPage' }  activeClassName='active'> <button>LEGO STARWARS</button> </NavLink>     
             </div> 
 
             <div className="categories">
-            <img src="https://torrentsgames.org/wp-content/uploads/2017/01/Lego-City-Undercover-Xbox360.jpg" width={300} height={200}/>
+            <NavLink to={ '/product' }  activeClassName='active'> <button><img src="https://torrentsgames.org/wp-content/uploads/2017/01/Lego-City-Undercover-Xbox360.jpg" width={300} height={200}/></button> </NavLink>
             <br/>
             <NavLink to={ '/product' }  activeClassName='active'> <button>LEGO CITY</button> </NavLink>     
             </div> 
 
             <div className="categories">
-            <img src="https://www.parentmap.com/images/article/6670/kids-with-lego.jpg" width={300} height={200}/>
+            <NavLink to={ '/KidsSets' }  activeClassName='active'> <button><img src="https://www.parentmap.com/images/article/6670/kids-with-lego.jpg" width={300} height={200}/></button> </NavLink>
             <br/>
             <NavLink to={ '/KidsSets' }  activeClassName='active'> <button>LEGO KIDS</button> </NavLink>     
             </div> 
