ebf72c16d35ac4d5206dec1d369a8cc1d4f948d2
catalinaandreitacarstoiu <0925887@hr.nl>
2017-12-10 17:46:37 +0100

    wishlist rendert, alleen nog items verwijderen

--- ClientApp/components/Wishlist.tsx
+++ ClientApp/components/Wishlist.tsx
@@ -20,29 +20,37 @@ export async function get_correctuser(user_id: number): Promise<Models.Wishlist[
 
 type WishlistRouterState = { legopr: Models.Lego[], userStatus: "Ingelogd" | 'Uitgelogd', user:Models.Users | "loading", wishlist2:Models.Wishlist[]}
 
-export class WishlistRouter extends React.Component<RouteComponentProps<{ wishlist:number}>, WishlistRouterState> {
-    constructor(props: RouteComponentProps<{wishlist:number}>) {
+export class WishlistRouter extends React.Component<RouteComponentProps<{ wishlist:number, lego:Models.Lego}>, WishlistRouterState> {
+    constructor(props: RouteComponentProps<{wishlist:number, lego:Models.Lego}>) {
         super(props)
         this.state = { legopr: [], userStatus:"Uitgelogd", user:"loading", wishlist2:[]}
     }
 
     componentWillMount() {
-        console.log("componentwillmount", this.state.wishlist2)
-        this.state.wishlist2 != null? 
-        get_correctuser(1).then(pr => this.setState({...this.state, wishlist2:pr.concat(this.state.wishlist2)})).catch(error => console.error(error))
-        :
-            null
-
         let prevList = localStorage.getItem("wishlist")
         let currentList = prevList == null ? null : prevList.split(",")
-        console.log({ currentList })
 
-        currentList != null? currentList.map(b =>
-            get_correctproduct(b).then(b => this.setState({ ...this.state, legopr: this.state.legopr.concat(b) }))
-                .catch(error => console.error(error))
+        sessionStorage.getItem("userStatus") != "Ingelogd"?
+        
 
-        )
+            currentList != null
+            ? currentList.map(b =>
+                get_correctproduct(b).then(b => this.setState({ ...this.state, legopr: this.state.legopr.concat(b) }))
+                    .catch(error => console.error(error)))
+            
             : null
+        
+        :
+        
+        this.state.wishlist2 != null && sessionStorage.getItem("userStatus") == "Ingelogd"? 
+        (get_correctuser(parseInt(sessionStorage.getItem("user"))).
+            then(pr => this.setState({...this.state, wishlist2:pr.concat(this.state.wishlist2)}, 
+                () => this.state.wishlist2.map((p: Models.Wishlist) => get_correctproduct(p.item_Number).
+                then(p => this.setState({...this.state, legopr:this.state.legopr.concat(p)})))))).
+                catch(error => console.error(error))
+        :
+            null        
+           
     }
 
     deleteItem(NextState: any)
@@ -65,17 +73,12 @@ export class WishlistRouter extends React.Component<RouteComponentProps<{ wishli
 
 
     render() {
-        console.log(this.state.wishlist2)
+        console.log(this.state.legopr)
         return <div>
             
-            {sessionStorage.getItem("userStatus") == "Ingelogd"?
-            <div>hi</div>
-            //    this.state.wishlist2.map(
-            //        (lego: Models.Wishlist) => <Wishlist load={lego} id={lego.item_Number} deleteItem={(p) => this.deleteItem(p)}/>)
-            :
-                this.state.legopr.map((lego: Models.Lego) =>
-                <Wishlist load={lego} id={lego.item_Number} deleteItem={(p) => this.deleteItem(p)} />)
-            }
+                {this.state.legopr.map((lego: Models.Lego) =>
+                <Wishlist load={lego} id={lego.item_Number} deleteItem={(p) => this.deleteItem(p)} />)}
+            
             
         </div>
     }
@@ -84,10 +87,10 @@ export class WishlistRouter extends React.Component<RouteComponentProps<{ wishli
 type WishlistProps = { id: number }
 
 type LoadProducts = { load: Models.Lego, id: string, deleteItem: (index: string) => void }
-export class Wishlist extends React.Component<LoadProducts, {bla : Models.Lego | "loading"}> {
+export class Wishlist extends React.Component<LoadProducts, {}> {
     constructor(props: LoadProducts) {
         super(props);
-        this.state = {bla: "loading"};
+        this.state = {};
     }
 
     componentWillUpdate(NextProps: any, NextState: any) {
