5088209182766f4cfcdd665eb83deadd6305988b
aivytruong <0938055@hr.nl>
2018-01-16 01:45:37 +0100

    Added homepage with 'special' products of the month

--- ClientApp/components/Home.tsx
+++ ClientApp/components/Home.tsx
@@ -2,11 +2,17 @@ import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import { Link, NavLink } from 'react-router-dom';
 import * as Models from './lego_types'
+import {ProductLoad} from './ProductLoad';
+import {Search} from './SearchFunction';
 
-export type HomeState = { lego: Models.Lego[] | "loading" }
 
-export async function get_products(): Promise<Models.Lego[]> {
-    let res = await fetch(`./custom/Product`, { method: 'get', credentials: 'include', headers: { 'content-type': 'application/json' } })
+type HomeComponentProps = {}
+type HomeState = { lego: Models.Lego[] | "loading" }
+type LoadProducts = { load: Models.Lego }
+
+
+export async function get_friendsproduct(theme:string): Promise<Models.Lego[]> {
+    let res = await fetch(`./custom/PromotionalSets/${theme}`, { method: 'get', credentials: 'include', headers: { 'content-type': 'application/json' } })
     let json = await res.json()
     console.log("received correct products", json)
     return json
@@ -19,110 +25,34 @@ export class Home extends React.Component<RouteComponentProps<{}>, HomeState> {
     }
 
     componentWillMount() {
-        get_products().then(pr => this.setState({ ...this.state, lego: pr }))
+        get_friendsproduct("Promotional").then(products => this.setState({ ...this.state, lego: products }))
+        console.log("mapping", this.state.lego)
     }
 
     render() {
 
-        // (function () {
 
-        //     function Slideshow(element) {
-        //         this.el = document.querySelector(element);
-        //         this.init();
-        //     }
-
-        //     Slideshow.prototype = {
-        //         init: function () {
-        //             this.wrapper = this.el.querySelector(".slider-wrapper");
-        //             this.slides = this.el.querySelectorAll(".slide");
-        //             this.previous = this.el.querySelector(".slider-previous");
-        //             this.next = this.el.querySelector(".slider-next");
-        //             this.index = 0;
-        //             this.total = this.slides.length;
-        //             this.timer = null;
-
-        //             this.action();
-        //             this.stopStart();
-        //         },
-        //         _slideTo: function (slide) {
-        //             var currentSlide = this.slides[slide];
-        //             currentSlide.style.opacity = 1;
-
-        //             for (var i = 0; i < this.slides.length; i++) {
-        //                 var slide = this.slides[i];
-        //                 if (slide !== currentSlide) {
-        //                     slide.style.opacity = 0;
-        //                 }
-        //             }
-        //         },
-        //         action: function () {
-        //             var self = this;
-        //             self.timer = setInterval(function () {
-        //                 self.index++;
-        //                 if (self.index == self.slides.length) {
-        //                     self.index = 0;
-        //                 }
-        //                 self._slideTo(self.index);
-
-        //             }, 5000);
-        //         },
-        //         stopStart: function () {
-        //             var self = this;
-        //             self.el.addEventListener("mouseover", function () {
-        //                 clearInterval(self.timer);
-        //                 self.timer = null;
-
-        //             }, false);
-        //             self.el.addEventListener("mouseout", function () {
-        //                 self.action();
-
-        //             }, false);
-        //         }
-
-
-        //     };
-
-        //     document.addEventListener("DOMContentLoaded", function () {
-
-        //         var slider = new Slideshow("#main-slider");
-
-        //     });
-        // })();
+            {/* Working on the homepage!
+            <div className="categories">
+            <NavLink to={ '/PromotionalSets' }  activeClassName='active'> <button><img src="http://maoincheoil.com/wp-content/uploads/2015/04/Special_Promotions_Header.png" width={300} height={200}/></button> </NavLink>
+            <br/>
+            <NavLink to={ '/PromotionalSets' }  activeClassName='active'> <button>Promotional</button> </NavLink>     
+            </div>   */} {
+            if (this.state.lego == "loading") return <div>loading...</div>
+        else
         return <div>
-            Working on the homepage!
-            {/* {this.state.lego == "loading" ?
-            <div>loading...</div>
-            :
-            this.state.lego.map(p => <HomeLoad lego={p}/>)} */}
-            {/* <h1> Most wanted! Last chance to buy! </h1>
-            <div className="slider-wrapper">
-                
-                <img src="https://pbs.twimg.com/media/Cl9q5-1WYAAfHL7.jpg:large" alt="First" className="slide" />
-                <img src="http://cdn-static.denofgeek.com/sites/denofgeek/files/styles/main_wide/public/2017/05/lego_main.jpg?itok=OYmDXcmD" alt="Second" className="slide" />
-                <img src="https://torrentsgames.org/wp-content/uploads/2017/01/Lego-City-Undercover-Xbox360.jpg" alt="Third" className="slide" />
-            </div> */}
-
+                        <h1>What's new!</h1>
+            <h3>Take a look at these special items in our store. Each month new and special items will take the spotlight. </h3>
+            <Search products={this.state.lego}/>
+           
+            {console.log("render", this.state.lego)}
         </div>
+            }
 
     }
 }
 
 
-export type HomeProps = { lego: Models.Lego }
-export class HomeLoad extends React.Component<HomeProps, {}> {
-    constructor(props, context) {
-        super(props),
-            this.state = {}
-    }
 
-    render() {
-        return <div>
-            
-            {/* <p><h1>What's new?</h1></p>
-            <NavLink to={'/Newproduct1'} activeClassName='active'> <button><img src={this.props.lego.image_URL} width={300} height={200} /></button> </NavLink>
-            <NavLink to={'/Newproduct2'} activeClassName='active'> <button><img src={this.props.lego.image_URL} width={300} height={200} /></button> </NavLink>
-            <NavLink to={'/Newproduct3'} activeClassName='active'> <button><img src={this.props.lego.image_URL} width={300} height={200} /></button> </NavLink> */}
 
-        </div>
-    }
-}
+
