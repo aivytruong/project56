65207beba931d9601f25e944086ed05cb2b7561f
aivytruong <0938055@hr.nl>
2018-01-16 14:57:13 +0100

    fixed homepage

--- ClientApp/components/Home.tsx
+++ ClientApp/components/Home.tsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import { Link, NavLink } from 'react-router-dom';
 import * as Models from './lego_types'
-import {ProductLoad} from './ProductLoad';
-import {Search} from './SearchFunction';
+import { ProductLoad } from './ProductLoad';
+import { Search } from './SearchFunction';
+import { get_product } from 'ClientApp/Components/AdminMode';
 
 
 type HomeComponentProps = {}
@@ -11,7 +12,7 @@ type HomeState = { lego: Models.Lego[] | "loading" }
 type LoadProducts = { load: Models.Lego }
 
 
-export async function get_friendsproduct(theme:string): Promise<Models.Lego[]> {
+export async function get_friendsproduct(theme: string): Promise<Models.Lego[]> {
     let res = await fetch(`./custom/PromotionalSets/${theme}`, { method: 'get', credentials: 'include', headers: { 'content-type': 'application/json' } })
     let json = await res.json()
     console.log("received correct products", json)
@@ -27,32 +28,47 @@ export class Home extends React.Component<RouteComponentProps<{}>, HomeState> {
     componentWillMount() {
         get_friendsproduct("Promotional").then(products => this.setState({ ...this.state, lego: products }))
         console.log("mapping", this.state.lego)
+
+
     }
 
     render() {
 
-
-            {/* Working on the homepage!
-            <div className="categories">
-            <NavLink to={ '/PromotionalSets' }  activeClassName='active'> <button><img src="http://maoincheoil.com/wp-content/uploads/2015/04/Special_Promotions_Header.png" width={300} height={200}/></button> </NavLink>
-            <br/>
-            <NavLink to={ '/PromotionalSets' }  activeClassName='active'> <button>Promotional</button> </NavLink>     
-            </div>   */} {
-            if (this.state.lego == "loading") return <div>loading...</div>
-        else
         return <div>
-                        <h1>What's new!</h1>
-            <h3>Take a look at these special items in our store. Each month new and special items will take the spotlight. </h3>
-            <Search products={this.state.lego}/>
-           
-            {console.log("render", this.state.lego)}
+            <h1>What's new!</h1>
+            <h3>Take a look at these special items in our store. Each month new and special items will take the spotlight. Check them out below! </h3>
+
+            {
+                <div className="categories">
+                    <NavLink to={'DetailProduct/850486'} activeClassName='active'> <button><img src="http://images.brickset.com/sets/images/850486-1.jpg" width={300} height={200} /></button> </NavLink>
+                    <br />
+                    <br/>
+                    <NavLink to={'DetailProduct/8805'} activeClassName='active'> <button><img src="http://images.brickset.com/sets/images/8805-1.jpg" width={300} height={200} /></button> </NavLink>
+                    <br />
+                    <br/>
+                    <NavLink to={'DetailProduct/45008'} activeClassName='active'> <button><img src="http://images.brickset.com/sets/images/45008-1.jpg" width={300} height={200} /></button> </NavLink>
+                    <br />
+                    <br/>
+                    <NavLink to={'DetailProduct/21004'} activeClassName='active'> <button><img src="http://images.brickset.com/sets/images/21004-1.jpg" width={300} height={200} /></button> </NavLink>
+                    <br />
+                    <br/>
+                    <NavLink to={'DetailProduct/41109'} activeClassName='active'> <button><img src="http://images.brickset.com/sets/images/41109-1.jpg" width={300} height={200} /></button> </NavLink>
+                </div>}
         </div>
-            }
+        {
+            // if (this.state.lego == "loading") return <div>loading...</div>
+            // else
+            //     return <div>
+
+
+            //         {/* {console.log("render", this.state.lego)} */}
+            //     </div>
+        }
 
     }
+
+
+
+
+
 }
-
-
-
-
-
