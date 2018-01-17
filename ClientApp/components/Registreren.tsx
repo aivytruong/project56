a32898bf54956ee15256a294358cd439502f5ae9
mnguyen93 <0893193@hr.nl>
2018-01-17 11:32:29 +0100

    register pattern fix complete i think?

--- ClientApp/components/Registreren.tsx
+++ ClientApp/components/Registreren.tsx
@@ -120,11 +120,11 @@ checkout(){
                     <div className='css-container'>
                         <p>
                             <span className='css-text-red'>First Name:</span>
-                            <input className='css-input css-lightred' pattern="[A-Za-z]*" title="Can only contain letters and numbers" value={this.state.firstName} onChange={event => this.setState({ ...this.state, firstName: event.target.value })} />
+                            <input className='css-input css-lightred' pattern="[A-Za-z]*" title="Can only contain letters" value={this.state.firstName} onChange={event => this.setState({ ...this.state, firstName: event.target.value })} />
                         </p>
                         <p>
                             <span className='css-text-red'>Last Name:</span>
-                            <input className='css-input css-lightred' pattern="[A-Za-z]" title="Can only contain letters and numbers" value={this.state.lastName} onChange={event => this.setState({ ...this.state, lastName: event.target.value })} />
+                            <input className='css-input css-lightred' pattern="[A-Za-z]*" title="Can only contain letters" value={this.state.lastName} onChange={event => this.setState({ ...this.state, lastName: event.target.value })} />
                         </p>
                         <p>
                             <span className='css-text-red'>User Name:</span>
@@ -132,31 +132,31 @@ checkout(){
                         </p>
                         <p>
                             <span className='css-text-red'>Email adress:</span>
-                            <input className='css-input css-lightred' pattern="[[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$]" title="must be a correct email" value={this.state.emailAdress} onChange={event => this.setState({ ...this.state, emailAdress: event.target.value })} />
+                            <input className='css-input css-lightred' pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" title="must be a correct email" value={this.state.emailAdress} onChange={event => this.setState({ ...this.state, emailAdress: event.target.value })} />
                         </p>
                         <p>
                             <span className='css-text-red'>Password:</span>
-                            <input className='css-input css-lightred' pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" value={this.state.password} onChange={event => this.setState({ ...this.state, password: event.target.value })} />
+                            <input className='css-input css-lightred' pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}$" title="Must include one number, one uppercase, lowercase letter and 8-16 characters" value={this.state.password} onChange={event => this.setState({ ...this.state, password: event.target.value })} />
                         </p>
                         <p>
-                            <span className='css-text-red'>Adress:</span>
-                            <input className='css-input css-lightred' value={this.state.adress} onChange={event => this.setState({ ...this.state, adress: event.target.value })} />
+                            <span className='css-text-red'>Zipcode + housenumber:</span>
+                            <input className='css-input css-lightred'pattern="[0-9]{4}\s*[a-zA-Z]{2}\s{1}[0-9a-zA-Z]{1,9}" title="Must contain a dutch zipcode and house number example 1234aa 19b or 1234 AA 19"  value={this.state.adress} onChange={event => this.setState({ ...this.state, adress: event.target.value })} />
                         </p>
                         <p>
                             <span className='css-text-red'>Phone Number:</span>
-                            <input className='css-input css-lightred' value={this.state.phoneNumber} onChange={event => this.setState({ ...this.state, phoneNumber: event.target.value })} />
+                            <input className='css-input css-lightred'pattern="^\+[0-9]{6,14}" title="Your number should be like this example: +312345678, 312345678 or 0612345678" value={this.state.phoneNumber} onChange={event => this.setState({ ...this.state, phoneNumber: event.target.value })} />
                         </p>
                         <p>
-                            <span className='css-text-red'>Country:</span>
-                            <input className='css-input css-lightred' value={this.state.Country} onChange={event => this.setState({ ...this.state, Country: event.target.value })} />
+                            <span className='css-text-red'>Country code:</span>
+                            <input className='css-input css-lightred' pattern="[A-Za-z]{3}" title=" Please enter your country code example: USA, ENG, CHN, JPN" value={this.state.Country} onChange={event => this.setState({ ...this.state, Country: event.target.value })} />
                         </p>
                         <p>
                             <span className='css-text-red'>Date of Birth(DD-MM-YY): </span>
-                            <input className='css-input css-lightred' value={this.state.Date_of_birth} onChange={event => this.setState({ ...this.state, Date_of_birth: event.target.value })} />
+                            <input className='css-input css-lightred' pattern="^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$" title="please enter a correct Date of Birth" value={this.state.Date_of_birth} onChange={event => this.setState({ ...this.state, Date_of_birth: event.target.value })} />
                         </p>
                         <p>
                             <span className='css-text-red'>Gender(M/V):</span>
-                            <input className='css-input css-lightred' value={this.state.gender} onChange={event => this.setState({ ...this.state, gender: event.target.value })} />
+                            <input className='css-input css-lightred' pattern="/^m$|^f$|^M$|^F$/" title="please enter m, f, M or F" value={this.state.gender} onChange={event => this.setState({ ...this.state, gender: event.target.value })} />
                         </p>
                         <p>
                             <button className='css-btn' onClick={() => this.Registreren()}>Create account</button>
